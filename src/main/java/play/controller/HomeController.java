24238a7bbefccc82a9ab7442145f05c251537464
PietroR28 <pietro.rizzo@studio.unibo.it>
2025-02-18 14:19:52 +0100

    lavoro grafico
    Signed-off-by: PietroR28 <pietro.rizzo@studio.unibo.it>

--- src/main/java/play/controller/HomeController.java
+++ src/main/java/play/controller/HomeController.java
@@ -2,9 +2,12 @@ package play.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
 public class HomeController {
+
+    @FXML
+    private Label welcomeLabel;
 
     @FXML
     private Button exercise1Button;
@@ -13,7 +16,14 @@ public class HomeController {
     private Button exercise2Button;
 
     @FXML
-    private Button logoutButton;
+    private Button exercise3Button;
+
+    @FXML
+    private Button exercise4Button;
+
+    public void setWelcomeMessage(String username) {
+        welcomeLabel.setText("Ciao, " + username + "!");
+    }
 
     @FXML
     public void handleExercise1() {
@@ -27,4 +37,15 @@ public class HomeController {
         System.out.println("Esercizio 2 selezionato");
     }
 
+    @FXML
+    public void handleExercise3() {
+        // Logica per gestire l'esercizio 3
+        System.out.println("Esercizio 3 selezionato");
+    }
+
+    @FXML
+    public void handleExercise4() {
+        // Logica per gestire l'esercizio 4
+        System.out.println("Esercizio 4 selezionato");
+    }
 }
