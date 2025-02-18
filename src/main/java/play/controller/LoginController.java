5fd2bfa33bd40ddde7f25ebd09628c30505d1ec5
PietroR28 <pietro.rizzo@studio.unibo.it>
2025-02-18 13:23:22 +0100

    homepage
    Signed-off-by: PietroR28 <pietro.rizzo@studio.unibo.it>

--- src/main/java/play/controller/LoginController.java
+++ src/main/java/play/controller/LoginController.java
@@ -1,17 +1,24 @@
 package play.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import play.model.UserDataManager;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML
     private TextField usernameField;
+
     @FXML
     private PasswordField passwordField;
+
     @FXML
     private Label statusLabel;
 
@@ -21,7 +28,15 @@ public class LoginController {
         String pass = passwordField.getText();
         if (UserDataManager.checkUser(user, pass)) {
             statusLabel.setText("Login Success!");
-            // Passa alla HomeScreen o gestisci la navigazione
+            // Passa alla schermata iniziale
+            try {
+                Stage stage = (Stage) usernameField.getScene().getWindow();
+                Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/Home.fxml")));
+                stage.setScene(scene);
+                stage.setTitle("Home - Play Esercizi");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
             statusLabel.setText("Credenziali errate.");
         }
